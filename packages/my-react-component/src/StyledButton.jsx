256c9d48e641069e5703c25f2a1d929880a5ad98
Ben Liew <bliew@splunk.com>
2026-03-18 17:29:29 +1100

    fix: Fix styling of buttons which were broken when use inside of Table.Row actions. Temporary fix of using !important in StyledButton

--- packages/my-react-component/src/StyledButton.jsx
+++ packages/my-react-component/src/StyledButton.jsx
@@ -1,9 +1,40 @@
 import styled from "styled-components";
 import Button from "@splunk/react-ui/Button";
+import {variables} from "@splunk/themes";
+
+function appearanceToBackgroundColor(appearance) {
+    if(appearance === "destructive"){
+        return variables.actionColorBackgroundDestructive;
+    }
+    if(appearance === "secondary"){
+        return variables.actionColorBackgroundSecondary;
+    }
+    return variables.actionColorBackgroundPrimary;
+}
+
+function appearanceToHoverBackgroundColor(appearance) {
+    if(appearance === "destructive"){
+        return variables.actionColorBackgroundDestructiveHover;
+    }
+    if(appearance === "secondary"){
+        return variables.actionColorBackgroundSecondaryHover;
+    }
+    return variables.actionColorBackgroundPrimaryHover;
+}
+
+// TODO: Handle disabled button styling (not needed currently)
 
 export const StyledButton = styled(Button)`
     flex-grow: 0;
     margin-left: ${props => (props.noMargin ? 0 : undefined)} !important;
     border-width: ${props => (props.noBorder ? 0 : undefined)};
     border-color: ${props => (props.borderColor ?? undefined)};
+
+    background-color: ${props => appearanceToBackgroundColor(props?.appearance)} !important;
+    color: ${variables.white} !important;
+
+
+    &:hover {
+        background-color: ${props => appearanceToHoverBackgroundColor(props?.appearance)} !important;
+    }
 `;
--- packages/my-react-component/src/buttons/DeleteIconOnlyButton.jsx
+++ packages/my-react-component/src/buttons/DeleteIconOnlyButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import BaseButton from "../BaseButton";
 import Tooltip from '@splunk/react-ui/Tooltip';
 import TrashCanCross from "@splunk/react-icons/TrashCanCross";
+import {variables} from "@splunk/themes";
+import BaseButton from "../BaseButton";
 
 export default function DeleteIconOnlyButton({onClick, tooltipContent = "Delete", ...props}) {
     return (
         <Tooltip content={tooltipContent} onClick={onClick}>
-            <BaseButton noMargin inline appearance="destructive" icon={<TrashCanCross hideDefaultTooltip/>} {...props}/>
+            <BaseButton borderColor={variables.contentColorMuted} noMargin inline appearance="destructive" icon={<TrashCanCross hideDefaultTooltip/>} {...props}/>
         </Tooltip>
     );
 }
